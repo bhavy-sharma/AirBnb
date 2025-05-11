84b3d3786ad98d910bb2f5ab5a7f9d9fff14007b
bhavy-sharma <bhavymay18@gmail.com>
2025-05-12 01:30:49 +0530

    Add Sample Data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Listing = require("./models/listing.js");
 
 const app = express();
 
@@ -13,8 +14,24 @@ async function main() {
 
 app.get("/", (req,res) => {
     res.send("Bhavy Sharma")
-})
+});
+
+app.get("/testListing", async (req,res) => {
+    let sampleListing = new Listing({
+        title : "My Home",
+        description : "My Home is Very Sweet Home and i Loved it...",
+        price : 12000,
+        location : "Hansi, Haryana",
+        country : "India"
+    });
+    await sampleListing.save();
+    console.log("Sample was Saved");
+    res.send("Successful");
+    
+});
+
+
 
 app.listen(8080, () => {
     console.log("Server is running on Port 8080");
-})
+});
