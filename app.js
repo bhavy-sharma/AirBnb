209adace274c377615c1305a66d67551fb34f4f5
bhavy-sharma <bhavymay18@gmail.com>
2025-05-13 16:10:43 +0530

    Edit & Update Route

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
+
 
 // Express App Initilize
 const app = express();
@@ -12,7 +14,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'));
 
 
 // DB se Connect krne ka Function
@@ -74,6 +76,22 @@ app.post("/listings", async (req, res) => {
     let newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect(`/listings`);
+});
+
+
+// Ye hai Babu Bhaiya Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+});
+
+
+// Ye hai Babu Bhaiya Update Route
+app.put("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    const updatedListing = await Listing.findByIdAndUpdate(id, req.body.listing);
+    res.redirect(`/listings/${id}`);
 });
 
 
